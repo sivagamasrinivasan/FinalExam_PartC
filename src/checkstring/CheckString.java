7d0ea29ff13f2d04683ba38d12bc4e5e53865211
sivagamasheridan <46792094+sivagamasheridan@users.noreply.github.com>
2020-04-15 11:52:49 -0400

    Update CheckString.java

--- src/checkstring/CheckString.java
+++ src/checkstring/CheckString.java
@@ -8,7 +8,7 @@ package checkstring;
 /** This class to checks the given string has only characters or not.
  * Complete this method and test this method in JUnit.
  * @author srinivsi
- */
+ April 13th */
 public class CheckString {
 
     /**
