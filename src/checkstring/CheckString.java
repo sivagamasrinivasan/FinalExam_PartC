1fe089b51a38f6527bab87b9cc5f03cc420571cd
sivagamasheridan <46792094+sivagamasheridan@users.noreply.github.com>
2022-04-20 12:30:23 -0400

    Update CheckString.java

--- src/checkstring/CheckString.java
+++ src/checkstring/CheckString.java
@@ -7,7 +7,7 @@ package checkstring;
 
 /** This class to checks the given string has any numbers/digits or not.
  * Complete this method and test this method in JUnit.
- * @author srinivsi
+ * @author srinivsi April 20
   */
 public class CheckString {
 
